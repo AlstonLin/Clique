bf0e392a979a0a67910541f5b6c5b493234a0fee
Ayush Vaidya <vdaybeatz@gmail.com>
2015-08-22 04:19:25 -0400

    heroku6

--- db/migrate/20150820193504_add_username_bio_profile_picture_cover_picture_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddUsernameBioProfilePictureCoverPictureToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-    add_column :users, :bio, :text
-    add_column :users, :profile_picture, :image
-    add_column :users, :cover_picture, :image
-  end
-end
