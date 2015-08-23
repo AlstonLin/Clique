4ddf371bfe3226711cde89b470f0b95c81701c6e
Ayush Vaidya <vdaybeatz@gmail.com>
2015-08-23 18:50:41 -0400

    heroku17

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823182615) do
+ActiveRecord::Schema.define(version: 20150823215714) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "pin_id"
@@ -55,7 +55,32 @@ ActiveRecord::Schema.define(version: 20150823182615) do
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                        default: "", null: false
+    t.string   "encrypted_password",           default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.string   "profile_picture_file_name"
+    t.string   "profile_picture_content_type"
+    t.integer  "profile_picture_file_size"
+    t.datetime "profile_picture_updated_at"
+    t.string   "username"
+    t.string   "bio"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "youtube"
+    t.string   "website"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
