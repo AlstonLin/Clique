a360a38cafd48bf0da96a87d2f09967a0a01df54
Ayush Vaidya <vdaybeatz@gmail.com>
2015-08-30 16:39:57 -0400

    heroku26

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [:show]
 
   # GET /orders
   def index
