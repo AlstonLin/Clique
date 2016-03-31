13384372df2f9e690ffe444a270326ece9953d0a
Alston Lin <alstonlin812@gmail.com>
2016-03-30 22:14:30 -0400

    Merge fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -80,28 +80,24 @@ $(document).on('ready pjax:success', function() {
 });
 
 window.jQuery(function() {
-  // detect browser scroll bar width
-  var scrollDiv = $('<div class="scrollbar-measure"></div>')
-        .appendTo(document.body)[0],
-      scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-
   $(document)
     .on('hidden.bs.modal', '.modal', function(evt) {
       // use margin-right 0 for IE8
       $(document.body).css('margin-right', '');
       $("nav .container3").css('margin-right', '');
-      $("nav .logo").css('margin-right', '');
+      $("nav .logo").css('margin-left', '');
     })
     .on('show.bs.modal', '.modal', function() {
       // When modal is shown, scrollbar on body disappears.  In order not
       // to experience a "shifting" effect, replace the scrollbar width
       // with a right-margin on the body.
-      if ($(window).height() < $(document).height()) {
-        $(document.body).css('margin-right', scrollBarWidth + 'px');
-        $rightmpx = parseFloat($("nav .container3").css('margin-right')) + scrollBarWidth;
-        $("nav .container3").css('margin-right', $rightmpx + 'px');
-        $("nav .logo").css('margin-left', - (18 + scrollBarWidth) + 'px');
-      }
+      var scrollDiv = $('<div class="scrollbar-measure"></div>')
+            .appendTo(document.body)[0],
+            scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+      $(document.body).css('margin-right', scrollBarWidth + 'px');
+      $rightmpx = parseFloat($("nav .container3").css('margin-right')) + scrollBarWidth;
+      $("nav .container3").css('margin-right', $rightmpx + 'px');
+      $("nav .logo").css('margin-left', - (18 + scrollBarWidth) + 'px');
     });
 });
 
