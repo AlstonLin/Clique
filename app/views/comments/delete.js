15f4ccb4ba022be6090e0b3c5e75bcd872d32520
Alston Lin <alstonlin812@gmail.com>
2016-04-29 14:11:49 -0400

    Added comment field

--- app/views/comments/delete.js
+++ app/views/comments/delete.js
@@ -1,1 +1,7 @@
-$("##{comment_form_id @commentable}").html('<%= j render :partial => "comments/comments", :locals => { commentable: @commentable } %>');
+$("#flash-container").html('<%= escape_javascript(render :partial => "shared/flash_box") %>');
+var $track_comments = $('<%= "##{comment_form_id @commentable}-show" %>');
+if ($track_comments.size() > 0){
+  $track_comments.html('<%= j render :partial => "tracks/show_comments", :locals => { track: @commentable } %>')
+} else{
+  $('<%= "##{comment_form_id @commentable}" %>').html('<%= j render :partial => "comments/comments_list", :locals => { commentable: @commentable } %>');
+}
