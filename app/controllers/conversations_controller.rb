356ea77faaa8ee3b4b00b9cc2ff1229f72267bb4
Alston Lin <alstonlin812@gmail.com>
2016-08-28 16:10:00 -0400

    Fixup

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
   def show
     @conversation = Conversation.find(params[:id])
     # Defensive Check
-    unless current_user in @conversation.users
+    unless @conversation.users.include?(current_user)
       send_401
       return
     end
